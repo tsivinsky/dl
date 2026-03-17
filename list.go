c278035ebed7ccbe54fa68c935ba60232751d2a7
Daniil Tsivinsky <daniil@tsivinsky.com>
2026-03-17 22:20:43 +0300

    add latest commit info to list command

--- list.go
+++ list.go
@@ -3,7 +3,37 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
+	"os/exec"
+	"strings"
+	"text/tabwriter"
 )
+
+type commitInfo struct {
+	hash         string
+	relativeDate string
+}
+
+func getRepoInfo(repoPath string) (*commitInfo, error) {
+	cmd := exec.Command("git", "log", "-n", "1", `--pretty=format:%H;%ar`)
+	cmd.Dir = repoPath
+
+	b, err := cmd.Output()
+	if err != nil {
+		return nil, err
+	}
+
+	s := strings.Split(string(b), ";")
+	if len(s) != 2 {
+		return nil, fmt.Errorf("invalid info format")
+	}
+
+	info := &commitInfo{
+		hash:         s[0],
+		relativeDate: s[1],
+	}
+	return info, nil
+}
 
 func listCommand() error {
 	_, confFile, err := setupConfig()
@@ -16,8 +46,16 @@ func listCommand() error {
 		log.Fatal(err)
 	}
 
+	tw := tabwriter.NewWriter(os.Stdout, 0, 2, 4, ' ', 0)
+	defer tw.Flush()
 	for _, item := range conf.DL {
-		fmt.Printf("%s\n", item.Name)
+		info, err := getRepoInfo(item.Destination)
+		if err != nil {
+			fmt.Fprintf(tw, "%s\t\tfailed to retrieve latest commit\t[%v]\n", item.Name, err)
+			continue
+		}
+
+		fmt.Fprintf(tw, "%s\t\t%s\t%s\n", item.Name, info.hash, info.relativeDate)
 	}
 
 	return nil
